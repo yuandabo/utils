47985a5583bb9588d686bfd771ad75f89ad32719
yyb <45970965+yuandabo@users.noreply.github.com>
2024-12-31 10:14:15 +0800

    feat: 虚拟滚动

---
+++ vue/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import VisualList from "./components/VisualList.vue";
+import { ref } from "vue";
+
+const data = ref<any[]>([]);
+for (let i = 0; i < 1000; i++) {
+  data.value.push({ id: i, value: i });
+}
+</script>
+
+<template>
+  <div style="height: 100vh; width: 100vw">
+        
+    <VisualList :listData="data" :itemSize="100" />
+      
+  </div>
+</template>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  height: 100%;
+}
+</style>
